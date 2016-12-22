a55bf0ed1b26bf132ec6ac36f19c09bec4f08806
Jason Rodriguez <jason.rodriguez@byu.edu>
2016-12-22 09:02:50 -0700

    Capture Finished
    Still need to figure out how to upload the data.

--- keystroke_gatherer.js
+++ keystroke_gatherer.js
@@ -1,132 +1,92 @@
 var keystroke_gather = {
-    template: {
-      "id": null,                 //a unique identifier for the data collected ex timestamp-name 8 encoded
-      "startingEventType": null,  //what triggered the program to start recording eg. focusin
-      "endingEventType": null,    //what triggered the program to stop recording eg. focusout
-      "Target": null,             //The name of the person who is giving this info?
-      "keyEvents": [              //The collection of every keystroke gathered
-        {
-          "keyCode": null,
-          "Target": null,
-          "Timestamp": null,
-          "EventType": null
-        }
-      ]
-    },
-    data: {
-        "id": "d7af3c71-771b-4666-88ee-41f736e84db1",
-        "startingEventType": "focusin",
-        "endingEventType": "focusout",
-        "startTimestamp": 1481187131061,
-        "endTimestamp": 1481187149357,
-        "Target": "given-name",
-        "keyEvents": [{
-            "KeyCode": 65,
-            "Target": "given-name",
-            "Timestamp": 1481187135347,
-            "EventType": "keydown"
-        }, {
-            "KeyCode": 65,
-            "Target": "given-name",
-            "Timestamp": 1481187136048,
-            "EventType": "keyup"
-        }, {
-            "KeyCode": 66,
-            "Target": "given-name",
-            "Timestamp": 1481187136050,
-            "EventType": "keydown"
-        }, {
-            "KeyCode": 66,
-            "Target": "given-name",
-            "Timestamp": 1481187136175,
-            "EventType": "keyup"
-        }, {
-            "KeyCode": 67,
-            "Target": "given-name",
-            "Timestamp": 1481187136548,
-            "EventType": "keydown"
-        }, {
-            "KeyCode": 67,
-            "Target": "given-name",
-            "Timestamp": 1481187137158,
-            "EventType": "keyup"
-        }, {
-            "KeyCode": 68,
-            "Target": "given-name",
-            "Timestamp": 1481187137160,
-            "EventType": "keydown"
-        }, {
-            "KeyCode": 68,
-            "Target": "given-name",
-            "Timestamp": 1481187137298,
-            "EventType": "keyup"
-        }, {
-            "KeyCode": 69,
-            "Target": "given-name",
-            "Timestamp": 1481187137498,
-            "EventType": "keydown"
-        }, {
-            "KeyCode": 69,
-            "Target": "given-name",
-            "Timestamp": 1481187137607,
-            "EventType": "keyup"
-        }, {
-            "KeyCode": 70,
-            "Target": "given-name",
-            "Timestamp": 1481187137788,
-            "EventType": "keydown"
-        }, ]
-    },
-    init: function() {
-        console.log('This is where I will put an intro text to the document')
-        var loginDiv = document.createElement('div');
-        loginDiv.id = "keystroke_gather_login";
-        loginDiv.style = `width: 100%; height: 25px; display: flex; justify-content: space-between;`;
-        loginDiv.innerHTML = `Name<input type="text" name="user" id="keystroke_name">
+  keystrokeCount: 0,  //The keystroke Count of the current Session
+  loginData: {},  //The name and username used during login
+  data: {
+    "Id": null, //a unique identifier for the data collected ex timestamp-name-login base64 encoded
+    "StartingEventType": 'focusin', //what triggered the program to start recording eg. focusin
+    "EndingEventType": 'focusout', //what triggered the program to stop recording eg. focusout
+    "StartTimestamp": null,
+    "EndTimestamp": null,
+    "Target": null, //The name of the person who is giving this info?
+    "KeyEvents": [ //The collection of every keystroke gathered
+      //      "KeyCode": evt.keyCode,
+      //      "Target": evt.target.name,
+      //      "Timestamp": Date.now(),
+      //      "EventType": evt.type
+    ]
+  },
+  init: function() {
+    console.log('This is where I will put an intro text to the document')
+    var loginDiv = document.createElement('div');
+    loginDiv.id = "keystroke_gather_login";
+    loginDiv.style = `width: 100%; height: 25px; display: flex; justify-content: space-between;`;
+    loginDiv.innerHTML = `Name<input type="text" name="user" id="keystroke_name">
               Username<input type="text" name="username" id="keystroke_username">
-              <input type="submit" name="keystoke_login" onclick="keystroke_gather.login()">`;
-        document.body.prepend(loginDiv);
-        document.getElementById('keystroke_gather_login').onkeydown = function(e){
-          if(e.keyCode == 13) {
-              keystoke_gather.login();
-            }
-};
-    },
-    login: function() {
-        console.log('this ran');
-        var keystroke_login_name = document.getElementById('keystroke_name').value;
-        var keystroke_login_username = document.getElementById('keystroke_username').value;
-        var keystroke_login_data = {
-            keystroke_login_name,
-            keystroke_login_username
-        };
-        var elem = document.getElementById('keystroke_gather_login');
-        elem.parentNode.removeChild(elem);
-        keystroke_gather.runAnalysis(keystroke_gather.data);
+              <input type="submit" name="keystoke_login" id="keystroke_gather_login_submit" onclick="keystroke_gather.login()">`;
+    document.body.prepend(loginDiv);
 
-    },
-    runAnalysis: function(jsondata) {
-        document.addEventListener('keydown', keystroke_gather.key);
-        document.addEventListener('keyup', keystroke_gather.key);
-    },
-    key: function(evt) {
-      //we can get the timestamp from the event or from a date.now() function. Date.now could also be used to measure differences by what time they are typing.
-      console.log(evt);
-      var showPanel = document.getElementById('currentKey');
-      var keystroke_obj = {
-        "keyCode": evt.keyCode,
-        "Target": evt.target.localName,
-        "Timestamp": Date.now(),
-        "EventType": evt.type
-      };
-      keystroke_gather.template.keyEvents.push(keystroke_obj)
-      if (evt.type == 'keyup') {
-        showPanel.innerHTML = "";
+    document.getElementById('keystroke_gather_login').onkeydown = function(e) {
+      if (e.keyCode == 13) {
+        keystroke_gather.login();
       }
-      if (evt.type == 'keydown') {
-        showPanel.innerHTML = evt.key;
-      }
+    };
+  },
+  login: function() {
+    //Gather data of Name and username;
+    var keystroke_login_name = document.getElementById('keystroke_name').value;
+    var keystroke_login_username = document.getElementById('keystroke_username').value;
+    keystroke_gather.loginData = {
+      keystroke_login_name,
+      keystroke_login_username
+    };
+
+    //Delete Login Bar;
+    var elem = document.getElementById('keystroke_gather_login');
+    elem.parentNode.removeChild(elem);
+
+    //create Initial Session;
+    keystroke_gather.createSession();
+
+  },
+  createSession: function() {
+    keystroke_gather.data.Id = Date.now() + '-' + btoa(keystroke_gather.loginData.keystroke_login_name) + '-' + btoa(keystroke_gather.loginData.keystroke_login_username);
+
+    keystroke_gather.data.StartTimestamp = Date.now();
+    keystroke_gather.data.target = window.location.href;
+
+    console.log(keystroke_gather);
+    keystroke_gather.runAnalysis();
+  },
+  runAnalysis: function() {
+    document.addEventListener('keydown', keystroke_gather.key);
+    document.addEventListener('keyup', keystroke_gather.key);
+  },
+  key: function(evt) {
+    if(keystroke_gather.keystrokeCount >= 2000) {
+      keystroke_gather.keystrokeCount = 0;
+      keystroke_gather.uploadData();
     }
+    //we can get the timestamp from the event or from a date.now() function. Date.now could also be used to measure differences by what time they are typing.
+    //console.log(evt);
+    var showPanel = document.getElementById('currentKey');
+    var keystroke_obj = {
+      "KeyCode": evt.keyCode,
+      "Target": evt.target.name,
+      "Timestamp": Date.now(),
+      "EventType": evt.type
+    };
+    keystroke_gather.data.KeyEvents.push(keystroke_obj)
+
+    if (evt.type == 'keydown') {
+      keystroke_gather.keystrokeCount++;
+      showPanel.innerHTML = evt.key;
+    }
+  },
+  uploadData: function() {
+    keystroke_gather.data.EndTimestamp = Date.now();
+    console.log(JSON.stringify(keystroke_gather.data.keyEvents));
+    console.log('Insert data Upload Here')
+  }
 }
 
 document.addEventListener('DOMContentLoaded', keystroke_gather.init);
