d186d6ff83cfd621f771b2fa905f9d1dab3c7d15
Jason Rodriguez <jason.rodriguez@byu.edu>
2017-02-06 21:32:03 -0700

    getting user upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 //Compile all my javascript files into one master minified file that I reference in my main js file.
 //1. compile 2. Minify
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
@@ -13,17 +13,17 @@ gulp.task('browser-sync', function() {
         },
     });
 });
- 
-gulp.task('sass', function () {
-  return gulp.src('./lib/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./lib/css'))
-    .pipe(browserSync.reload({stream: true}));
+
+gulp.task('sass', function() {
+    return gulp.src('./lib/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./lib/css'))
+        .pipe(browserSync.reload({ stream: true }));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./lib/sass/**/*.scss', ['sass']);
-  gulp.watch('*html').on('change', browserSync.reload);
+
+gulp.task('sass:watch', function() {
+    //gulp.watch('./lib/sass/**/*.scss', ['sass']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload({ stream: true}));
 });
 
 gulp.task("default", ['browser-sync', 'sass', 'sass:watch']);
